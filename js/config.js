ba1a53b674a0097470c35d7b02873930bca96b10
narsinghpawar <67041191+narsinghpawar@users.noreply.github.com>
2026-02-12 11:57:53 +0530

    Code Changes

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ export const CONFIG = {
   REPO: "parking-system",
   FILE_PATH: "vehicles.json",
   TOKEN:
-    "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",
+    "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",
 };
